49aeed1e0ced0c5c88eacea69e142dc015cfe124
lenryk <lenry101@gmail.com>
2021-09-28 22:34:26 +0100

    added new archive template page

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,9 @@ const path = require("path")
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
+
+  const archiveTemplate = path.resolve("./src/templates/archive.js")
+
   const result = await graphql(`
     {
       wp {
@@ -30,5 +33,33 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  console.log(result)
+  const { wp, allWpCategory } = result.data
+
+  //Category pages
+  allWpCategory.edges.forEach(category => {
+    const postsPerPage = wp.readingSettings.postsPerPage
+    const numberOfPosts = category.node.count
+    const numPages = Math.ceil(numberOfPosts / postsPerPage)
+
+    //Some categories may be empty and we dont want to create pages for them
+    //Also dont want to create pages for uncategorized
+    if (numberOfPosts > 0 || category.node.name !== "uncategorized") {
+      Array.from({ length: numPages }).forEach((_, i) => {
+        createPage({
+          path: i === 0 ? category.node.uri : `${category.node.uri}${i + 1}`,
+          component: archiveTemplate,
+          context: {
+            limit: postsPerPage,
+            skip: i * postsPerPage,
+            numPages,
+            currentPage: i + 1,
+            catId: category.node.id,
+            catName: category.node.name,
+            catUri: category.node.uri,
+            categories: allWpCategory,
+          },
+        })
+      })
+    }
+  })
 }
